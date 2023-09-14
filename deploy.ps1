71ebc6bd4345a5839d174bf4f54dcd6965b085be
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-14 20:24:49 +0300

    Added an ErrorAction flag to the if block

--- deploy.ps1
+++ deploy.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'stop'
 $StackName = 'awsvpcstack'
 $TemplateBody = Get-Content -Path cloudformation-vpc.yml -Raw
 
-if (Get-CFNStack -StackName $StackName) {
+if (Get-CFNStack -StackName $StackName -ErrorAction Ignore) {
     Update-CFNStack -StackName $StackName -TemplateBody $TemplateBody
 }
 else {
