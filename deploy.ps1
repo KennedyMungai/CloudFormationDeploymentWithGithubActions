3b3ffcf44841108120d8c471e4d29c6646562fcd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-14 20:17:51 +0300

    Updated the powershell script

--- deploy.ps1
+++ deploy.ps1
@@ -2,4 +2,9 @@ $ErrorActionPreference = 'stop'
 $StackName = 'awsvpcstack'
 $TemplateBody = Get-Content -Path cloudformation-vpc.yml -Raw
 
-New-CFNStack -StackName $StackName -TemplateBody $TemplateBody
+if (Get-CFNStack -StackName $StackName) {
+    Update-CFNStack -StackName $StackName -TemplateBody $TemplateBody
+}
+else {
+    New-CFNStack -StackName $StackName -TemplateBody $TemplateBody
+}
